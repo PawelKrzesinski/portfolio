ddc199d1cb047ac8e45c8d7db08e2234fc3412f5
Pawel <pawel.krzesinski1989@gmail.com>
2022-09-25 17:07:25 +0100

    Changed the animation on the header and tidied up home section

--- src/components/section-home/section-home.component.js
+++ src/components/section-home/section-home.component.js
@@ -5,9 +5,9 @@ export default function SectionHome() {
 	return (
 		<div className="section-1" id="home">
 			<div className="filter">
-				<h2 className="h2-1"><span>Pawel Krzesinski</span></h2>
-				<h2 className="h2-2">Software Developer</h2>
-				<a href="#about"  className="button-about-me">More about me!</a>
+				<h2 className="h2-1">Pawel Krze<span>sin</span>ski</h2>
+				<h3 className="h2-2">Software Developer</h3>
+				<a href="#about" className="button-about-me">More about me!</a>
 			</div>
 		</div>
 	)
