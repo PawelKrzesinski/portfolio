5bf3eca9e22affb3ceb12c15eefcb20545e45168
Pawel <pawel.krzesinski1989@gmail.com>
2023-04-05 13:15:21 +0100

    Final version of server implementation. SendGrid API through SMTP connection

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-const sgMail = require('@sendgrid/mail')
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 const creds = require('./config');
-
+const nodemailer = require('nodemailer');
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const transport =  {
+	host: 'smtp.sendgrid.net',
+	port: 465,
+	auth: {
+		user: 'apikey',
+		pass: creds.APIKEY
+	}
+}
+
+const transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+	if(error){
+		console.log(error.message);
+		throw new Error(error.message)
+	} else {
+		console.log('Server is ready to take messages !');
+	}
+})
 
 router.post('/send', (req, res, next) => {
 	const payload = {
@@ -17,21 +36,26 @@ router.post('/send', (req, res, next) => {
 	  comment: req.body.comment,
 	}
 	const content = `Name: ${payload.name} \nSubject: ${payload.subject} \nE-mail: ${payload.email} \nComment: ${payload.comment}`;
-	sgMail.setApiKey(creds.PASS)
-	const msg = {
+	const mail = {
 		from: creds.USER,
 		to: creds.USER,
 		subject: 'New message from Contact Form',
 		text: content,
 	}
-	sgMail
-		.send(msg)
-		.then(() => {
-			console.log('Email sent')
-		})
-		.catch((error) => {
-			console.error(error)
-		})
+	transporter.sendMail(mail, (err, data) => {
+		if(err){
+			console.log('Something went wrong!', err.message);
+			res.json({
+				status: 'Failed!',
+				err: err
+			})
+		} else {
+			console.log('Sent successfully')
+			res.json({
+				status: 'Success!',
+			})
+		}
+	})
 })
 app.use('/api', router);
 
